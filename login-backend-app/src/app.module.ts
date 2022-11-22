eeeb541892842d3e57c071fa30c51c56f27b1a16
lesasi <nevinusa@gmail.com>
2022-11-22 07:54:13 +0530

    Added modules

--- login-backend-app/src/app.module.ts
+++ login-backend-app/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import config from './config/configuration';
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     load: [config]
-  })],
+  }), DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
